45879eb067e22d0b46981aaae6641fe9ec21f1a4
whc <1532106001@qq.com>
2020-07-23 19:11:54 +0800

    ;

---
+++ lib/ui/view/videoSearchPage.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class VideoSearchPage extends StatelessWidget {
+  const VideoSearchPage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("123"),
+    );
+  }
+}
